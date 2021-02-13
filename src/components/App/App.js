31659bd33ab0759a323e8b6b5c4a59b3bf698aae
Quinn Johnson <johnson.fargo87@gmail.com>
2021-02-13 01:36:33 -0600

    added routes for details and form

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,18 +1,22 @@
 import {HashRouter as Router, Route} from 'react-router-dom';
 import './App.css';
 import MovieList from '../MovieList/MovieList'
-
+import MovieDetails from '../MovieDetails/MovieDetails'
+import AddMovie from '../AddMovie/AddMovie'
 function App() {
   return (
     <div className="App">
       <h1>The Movies Saga!</h1>
-      <Router>        
+      <Router>      
         <Route path="/" exact>
           <MovieList />
         </Route>
-        
-        {/* Details page */}
-
+        <Route path='/details'>
+          <MovieDetails />
+        </Route>
+        <Route path='/addmovie'>
+          <AddMovie />
+        </Route>
         {/* Add Movie page */}
       </Router>
     </div>
