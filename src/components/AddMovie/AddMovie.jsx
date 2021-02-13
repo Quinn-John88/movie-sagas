f8fd9a541e95735d7a7648a9a927df128f676b3a
Quinn Johnson <johnson.fargo87@gmail.com>
2021-02-13 01:35:31 -0600

    added AddMovie folder and file with form included

---
+++ src/components/AddMovie/AddMovie.jsx
@@ -0,0 +1,30 @@
+function AddMovie() {
+    return (
+        <>
+            <form>
+                <input></input>
+                <input></input>
+                <textarea></textarea>
+                <select>
+                    <option>Adventure</option>
+                    <option>Animated</option>
+                    <option>Biographical</option>
+                    <option>Comedy</option>
+                    <option>Disaster</option>
+                    <option>Drama</option>
+                    <option>Epic</option>
+                    <option>Fantasy</option>
+                    <option>Musical</option>
+                    <option>Romantic</option>
+                    <option>Science Fiction</option>
+                    <option>Space-Opera</option>
+                    <option>Superhero</option>
+                </select>
+                <button>Save</button>
+                <button>Cancel</button>
+            </form>
+        </>
+    )
+}
+
+export default AddMovie;
